263073ba7a3b6de30d47edd468d76777fd5ee63e
Brayancrc <brayancrc@gmail.com>
2023-02-07 15:53:18 -0300

    initial commit

---
+++ SistemaBancario/src/negocio/IdadeNaoPermitidaException.java
@@ -0,0 +1,17 @@
+package negocio;
+
+/**
+ * Exce��o a ser lan�ada quando a idade de um poss�vel novo cliente n�o for
+ * aceita.
+ * 
+ * @author Brayan Cruz
+ */
+public class IdadeNaoPermitidaException extends Exception {
+
+	public static String MSG_IDADE_INVALIDA = "A idade do cliente precisa estar entre 18 e 65 anos.";
+
+	public IdadeNaoPermitidaException(String msg) {
+		super(msg);
+	}
+
+}
